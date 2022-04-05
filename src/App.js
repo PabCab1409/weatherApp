cb8da37066779a8db31182a9fe9d7deb27b19b24
Pablo Cabello <pcj1409@gmail.com>
2022-04-05 18:04:45 +0200

    intento hacerlo modular

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className='containerMenu'>
+    <div className="containerMenu">
       <ul>
-        <li><a href='#'>About</a></li>
-        <li><a href='#'>Map</a></li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Map</a>
+        </li>
       </ul>
-      <div className='containerSearcher'>
-        <input placeholder='Pablo' >
-        </input>
+      <div className="containerSearcher">
+        <input placeholder="Pablo"></input>
       </div>
-      <div className='containerStatsCity'>
-        asd
+      <h1>TOLEDO</h1>
+      <div className="containerStatsCity">
+        <div className="generalInfo">
+          <h1 className="degreesTitle">18 °C</h1>
+          <img className="weatherImg" src="https://www.metaweather.com/static/img/weather/s.svg"></img>
+        </div>
+        <div className="dayInfo">
+          asda
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/Container.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+const Container = (width, height) =>{
+
+    return(
+        <div>
+            <p></p>
+        </div>
+    )
+
+}
+
+export default Container
