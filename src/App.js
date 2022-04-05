1ce8537d284afa6af013eac83b6803437f290cc1
Pablo Cabello <pcj1409@gmail.com>
2022-04-05 14:07:30 +0200

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='containerMenu'>
+      <ul>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Map</a></li>
+      </ul>
+      <div className='containerSearcher'>
+        <input placeholder='Pablo' >
+        </input>
+      </div>
+      <div className='containerStatsCity'>
+        asd
+      </div>
     </div>
   );
 }
