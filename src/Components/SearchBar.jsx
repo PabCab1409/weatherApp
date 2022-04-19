c817f5298b9a3457cf4437076c88ecf12d660074
Pablo Cabello <pcj1409@gmail.com>
2022-04-19 15:16:35 +0200

    searchbar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../Styles/SearchBar.css"
+
 const SearchBar = () => {
   return (
     <form>
+      <button type="submit"></button>
       <input type="search" placeholder="search"></input>
-      <button type="submit">Search</button>
     </form>
   );
 };
