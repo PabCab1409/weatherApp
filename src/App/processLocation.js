cced82f15932d6ad3e7972b4f8fb9d12bba8d04c
Pablo Cabello <pcj1409@gmail.com>
2022-04-21 14:11:43 +0200

    si

--- src/App/processLocation.js
+++ src/App/processLocation.js
@@ -1,12 +1,5 @@
 const processLocation = () => {
-  var autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById("inputLocation"),
-    {
-      types: ["establishment"],
-      componentRestrictions: { country: ["AU"] },
-      fields: ["place_id", "geometry", "name"],
-    }
-  );
+  
 };
 
 export default processLocation;
--- src/Icons/Icons.jsx
+++ src/Icons/Icons.jsx
@@ -37,7 +37,16 @@ export function Visibility() {
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <circle cx="12" cy="12" r="2"></circle>
       <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
+      <defs>
+      <linearGradient id="Gradient1" x2="100%" y2="100%">
+        <stop offset="0%" stop-color="#fcd0d0" />
+        <stop offset="40%" stop-color="#f9aaaa" />
+        <stop offset="60%" stop-color="#f98787" />
+        <stop offset="100%" stop-color="#f96b6b" />
+      </linearGradient>
+    </defs> 
     </svg>
+    
   );
 }
 
