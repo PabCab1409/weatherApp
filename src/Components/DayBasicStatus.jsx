8311cb791009d84b03bff211c7cf9fa7710ce503
Pablo Cabello <pcj1409@gmail.com>
2022-04-27 14:04:58 +0200

    trying

--- src/Components/DayBasicStatus.jsx
+++ src/Components/DayBasicStatus.jsx
@@ -1,43 +1,38 @@
 import React, { useState, useEffect } from "react";
-import DaysForecast from "./DaysForecast"
 import Box from "./Box";
 import SimpleDayInfo from "./SimpleDayInfo";
 import ComplexDayInfo from "./ComplexDayInfo";
 import "../Styles/Spinner.css";
 
 const DayBasicStatus = () => {
-  var simplePath =
-    "https://www.metaweather.com/api/location/search/?query=london";
-  var finalJson;
-  const [isLoading, setLoading] = useState(true);
 
-  const fetchData = () => {
-    fetch(simplePath)
-      .then((response) => response.json())
-      .then((json) => {
-        var woeid = json["0"]["woeid"];
-        var complexPath = `https://www.metaweather.com/api/location/${woeid}/`;
-
-        fetch(complexPath)
-          .then((response) => response.json())
-          .then((data) => {
-            finalJson = data;
-            setLoading(false);
-            
-          })
-          .catch((error) => {
-            <p>{error}</p>
-          });
-      })
-      .catch((error) => {
-        <p>{error}</p>
-      });
-  };
+  const [json,setJson] = useState([]);
 
   useEffect(() => {
-    fetchData();
-  });
 
+    var simplePath =
+    "https://www.metaweather.com/api/location/search/?query=london";
+
+    fetch(simplePath)
+    .then((response) => response.json())
+    .then((json) => {
+      var woeid = json["0"]["woeid"];
+      var complexPath = `https://www.metaweather.com/api/location/${woeid}/`;
+      
+      fetch(complexPath)
+        .then((response) => response.json())
+        .then((json) => setJson(json))
+        .catch((error) => {
+          <p>{error}</p>
+        });
+    })
+    .catch((error) => {
+      <p>{error}</p>
+    });
+
+  },[]);
+
+  /*
   if (isLoading) {
     return (
       <div style={{ height: 200, justifyContent: "center", display: "flex" }}>
@@ -45,12 +40,12 @@ const DayBasicStatus = () => {
       </div>
     );
   }
-
+*/
   return (
     <Box
       props={
         <React.Fragment>
-          <SimpleDayInfo></SimpleDayInfo>
+          <SimpleDayInfo json={json}></SimpleDayInfo>
           <ComplexDayInfo></ComplexDayInfo>
         </React.Fragment>
       }
--- src/Components/SimpleDayInfo.jsx
+++ src/Components/SimpleDayInfo.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import "../Styles/SimpleDayInfo.css";
 
 const SimpleDayInfo = (props) => {
+
+  const [state,useState] = useState(false)
+
+  if(props.json != null | props.json != undefined){
+    useState(true)
+  }
+
+  if(!state){
+    return(<div>wait</div>)
+  }
+
+  var json = props.json
+
   return (
     <div className="generalInfo">
       <div className="iconState">
         <img
           style={{ width: 200 }}
-          src="https://www.metaweather.com/static/img/weather/hc.svg"
+          src="https://www.metaweather.com/static/img/weather/s.svg"
         ></img>
       </div>
       <div className="cityInfo">
-        <h1 className="cityDegree">18º C</h1>
-        <p>{props.message}</p>
+        <h1 className="cityDegree">{Math.round(json['consolidated_weather'][0]['the_temp']) }º C</h1>
         <h2>Toledo</h2>
         <p style={{ marginTop: 10 }}>Nublado</p>
         <div className="minMax">
